91487bfeace3c41f514f56fca4c39bc71a77febc
VELES <v.golyadkin2005@gmail.com>
2023-11-28 20:22:17 +0600

    Новая работа =]

---
+++ Hash_Registration/Hash_Registration/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hash_Registration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        async private void Form1_Load(object sender, EventArgs e)
+        {
+            Form1 F = new Form1();
+
+            //Form1 F = new Form1();
+            for (float j = 0; j <= 10; j++)
+            {
+                for (float i = 0; i < 1; i += 0.05f, await Task.Delay(10))
+                {
+                    this.Opacity = i;
+                }
+                for (float i = 1; i >= 0; i -= 0.05f, await Task.Delay(10))
+                {
+                    this.Opacity = i;
+                }
+            }
+            MessageBox.Show(Convert.ToString(F.Opacity));
+        }
+    }
+}
---
+++ Hash_Registration/Hash_Registration/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Hash_Registration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hash_Registration")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("4afe1a3a-d4d0-4d26-b47e-3e4779ec2539")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
